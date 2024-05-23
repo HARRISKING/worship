0f3ad051797a3eabd032de0304ff89e42b9909b4
suixing <suixing@uni-ubi.com>
2024-05-23 20:45:32 +0800

    feat: 框架搭建完成

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,3 @@
-/**
- * 首页
- */
 import React from 'react';
 import { View } from '@tarojs/components';
 
@@ -8,7 +5,16 @@ import type { FC } from 'react';
 import './index.less'
 const Page: FC = () => {
   const root='home'
-  return <View className={root}>2sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</View>;
+  return <View className={root}>
+    <View  className={`${root}-mammon`}></View>
+    <View  className={`${root}-processBox`}>
+      <View  className={`${root}-processBox-processItem`}>富强</View>
+      <View  className={`${root}-processBox-processItem`}>民主</View>
+      <View  className={`${root}-processBox-processItem`}>文明</View>
+      <View  className={`${root}-processBox-processItem`}>和谐</View>
+    </View>
+    <View  className={`${root}-person`}>跪拜区</View>
+  </View>;
 
 };
 
